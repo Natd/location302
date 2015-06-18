1d4d243dd0019e49821daf40f07cb56fbaf0c712
theNatd <theNatd@gmail.com>
2015-06-18 13:55:28 +0300

    refactored code, added Struct Location

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ import "github.com/natd/location302"
 location302.GetLink(1, "secret", "http://google.com")
 ```
 
+or you can build a Location Struct and get a link
+
+```go
+loc := location302.Location{}
+loc.SetId(1)
+loc.SetSecret("asdasdadww")
+loc.SetUrl("http://mail.ru")
+loc.GetLink()
+```
--- location302.go
+++ location302.go
@@ -9,7 +9,47 @@ import (
 	"net/url"
 )
 
+
+type Location struct {
+	id int
+	secret string
+	url string
+}
+
+func (l Location) Id() int {
+	return l.id
+}
+
+func (l *Location) SetId(value int) {
+	l.id = value
+}
+
+func (l Location) Secret() string {
+	return l.secret
+}
+
+func (l *Location) SetSecret(value string) {
+	l.secret = value
+}
+
+func (l Location) Url() string {
+	return l.url
+}
+
+func (l *Location) SetUrl(value string) {
+	l.url = value
+}
+
+
+func (l *Location) GetLink() string {
+	return generateLink(l.id, l.secret, l.url)
+}
+
 func GetLink(_id int, _secret string, _url string) string {
+	return generateLink(_id, _secret, _url)
+}
+
+func generateLink(_id int, _secret string, _url string) string {
 	const serviceUrl = "http://302-location.com"
 	redirectUrl := url.QueryEscape(_url)
 	buffer := bytes.Buffer{}
